cd8048e56e98f9a94d4ab67af3c2e71b2a75ba71
Villa <elvirwerk@hotmail.com>
2023-11-17 13:25:14 +0100

    Load the data and compute some statistics.

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,17 @@
 import pandas as pd
 
-credit_data = pd.read_csv("")
+credit_data = pd.read_csv("all_data.csv", delimiter=";")
+
+
+#IF YOU OPEN A NEW PYTHON FILE AND WANT TO USE THE credit_data, THEN WRITE THE FOLLOWING IN YOUR PYTHON FILE:
+    # from data_loader.py import credit_data
+
+#uncomment the following for more specific information.
+# print(credit_data.info())
+# print(credit_data.describe())
+# print(credit_data.head())
+# print(credit_data.shape)
+# print(credit_data.index)
+# print(credit_data.columns)
+# print(credit_data.dtypes)
+# print(credit_data)
